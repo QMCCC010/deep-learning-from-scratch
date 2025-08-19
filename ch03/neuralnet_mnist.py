1516ff6a265edbe80a0e1657a1b3fa56e32c6e0c
QMCCC <2868916930@qq.com>
2025-08-19 19:23:23 +0800

    添加注释

--- ch03/neuralnet_mnist.py
+++ ch03/neuralnet_mnist.py
@@ -3,8 +3,8 @@ import sys, os
 sys.path.append(os.pardir)  # 親ディレクトリのファイルをインポートするための設定
 import numpy as np
 import pickle
-from dataset.mnist import load_mnist
-from common.functions import sigmoid, softmax
+from dataset.mnist import load_mnist # type: ignore
+from common.functions import sigmoid, softmax # type: ignore
 
 
 def get_data():
